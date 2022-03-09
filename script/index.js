438f1b2d89747e8ca06e9ac81f675ee0c5c128c5
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-09 16:09:29 +0100

    aggiunta richiesta email

--- script/index.js
+++ script/index.js
@@ -1,1 +1,13 @@
-console.log('Ciao!')
+//JS MAIL
+
+//Chiedi l'e-mail all'utente
+
+const inputUtente = document.getElementById('input-utente');
+const logInButton = document.getElementById('accedi');
+
+logInButton.addEventListener('click', function(){
+    console.log('accedi');
+    const emailUtente = inputUtente.value;
+    console.log(emailUtente);
+
+})
